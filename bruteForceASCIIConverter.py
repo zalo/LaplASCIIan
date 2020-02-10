92f4aa3a54196b725e08f1b70b0964412ab212ad
Johnathon Selstad <makeshifted@gmail.com>
2020-02-09 17:09:10 -0800

    Update to use Consolas as its base font...

--- bruteForceASCIIConverter.py
+++ bruteForceASCIIConverter.py
@@ -1,24 +1,25 @@
 import numpy as np
 import cv2
 import math
+import pyperclip
+from PIL import ImageFont, ImageDraw, Image
 
-laplacian = False
-density = 5
-font_size_x = 8
-blur = 1 # must be odd
+laplacian = True
+density = 17
+font_size_y = 18
+blur = 5 # Must Be Odd
 
 # Load the original image
 oimg = cv2.imread("image.jpg")
-oimg = cv2.resize(oimg, (1080, int(oimg.shape[0] *(1080/oimg.shape[1]))))
+oimg = cv2.resize(oimg, (720, int(oimg.shape[0] *(720/oimg.shape[1]))))
 oimg = cv2.cvtColor(oimg, cv2.COLOR_BGR2GRAY)
-img = cv2.GaussianBlur(oimg, (5, 5), 0)
 
-img = cv2.bilateralFilter(img, 5, 20, 20)
+# These settings also control the fidelity of the output image...
+img = cv2.GaussianBlur(oimg, (blur, blur), 0)
+img = cv2.bilateralFilter(img, blur, 20, 20)
 img = cv2.Laplacian(img, cv2.CV_8U)
-img = cv2.bilateralFilter(img, 5, 200, 200)
-#img = cv2.medianBlur(img, 9)
+img = cv2.bilateralFilter(img, blur, 200, 200)
 
-#img = cv2.GaussianBlur(img, (5, 5), 0)
 img = cv2.normalize(img, img, 255, 0, cv2.NORM_MINMAX)
 img = img.astype(np.float) * (density / 255)
 
@@ -26,18 +27,25 @@ finalImage = np.zeros(img.shape, dtype=np.uint8)
 
 # Specify the font to create
 asciiCharacters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-font_size_y = int(font_size_x * 1.5)
+font_size_x = int(font_size_y * 0.45)
+consolas = ImageFont.truetype("./Consolas.ttf", int(font_size_y*0.8))
 
 asciiAtlas     = np.zeros((len(asciiCharacters), font_size_y, font_size_x), dtype=np.uint8)
 laplacianAtlas = np.zeros(asciiAtlas.shape, dtype=np.uint8)
 for i in range(len(asciiCharacters)):
-  cv2.putText(asciiAtlas[i], asciiCharacters[i], (int(font_size_x*0.2), font_size_x), cv2.FONT_HERSHEY_SIMPLEX, font_size_x/40, (255,255,255), 0, 0) #cv2.LINE_AA
+  # Make into PIL Image
+  im_p = Image.fromarray(asciiAtlas[i])
+  draw = ImageDraw.Draw(im_p)
+  draw.text((0, 3), asciiCharacters[i], (255), font=consolas)
+  asciiAtlas[i] = np.array(im_p)
+
   laplacianAtlas[i] = cv2.Laplacian(asciiAtlas[i], cv2.CV_8U) if laplacian else np.copy(asciiAtlas[i])
   laplacianAtlas[i] = cv2.GaussianBlur(laplacianAtlas[i], (3, 3), 0)
   laplacianAtlas[i] = laplacianAtlas[i].astype(np.float)
   cv2.normalize(laplacianAtlas[i], laplacianAtlas[i], 255, 0, cv2.NORM_MINMAX)
 
-# Extract patches...
+# Construct string...
+outputArt = ''
 for y in range(int(img.shape[0] / font_size_y)):
   for x in range(int(img.shape[1] / font_size_x)):
     imgSlice = img[y * font_size_y : (y + 1) * font_size_y, x * font_size_x : (x + 1) * font_size_x]
@@ -48,10 +56,15 @@ for y in range(int(img.shape[0] / font_size_y)):
     minIndex             = np.argmin(alphabetSimilarities)
 
     finalImage[y * font_size_y : (y + 1) * font_size_y, x * font_size_x : (x + 1) * font_size_x] = asciiAtlas[minIndex]
+    outputArt += asciiCharacters[minIndex]
+  outputArt += '\r\n'
+
+
+pyperclip.copy(outputArt)
 
 # Display finished images...
 cv2.imshow("Original Image", oimg)
-cv2.imshow("Original Image", img)
-cv2.imshow("ASCII Atlas", laplacianAtlas[56])
+cv2.imshow("Processed Image", img)
+cv2.imshow("ASCII Atlas", laplacianAtlas[33])
 cv2.imshow("ASCII Image", finalImage)
 cv2.waitKey(0)
