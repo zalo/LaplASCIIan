0aaadd6b113960d14ffb745a3ff0357d6c776c71
Johnathon Selstad <makeshifted@gmail.com>
2020-02-13 21:36:54 -0800

    Attempt to move to JSON for data transfer...

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 class handler(BaseHTTPRequestHandler):
 
@@ -8,8 +9,8 @@ class handler(BaseHTTPRequestHandler):
         self.end_headers()
 
         # Attempt to read the body of the request
-        body = self.rfile.read(int(self.headers.get("Content-Length")))
+        body = json.loads(self.rfile.read(int(self.headers.get("Content-Length"))))
 
-        message = 'Hello from Python from a ZEIT Now Serverless Function!   You submitted: '+str(body)
+        message = 'Hello from Python from a ZEIT Now Serverless Function!   You submitted: '+str(body["gifUrl"]) +" with a density of " + body["density"]
         self.wfile.write(message.encode())
         return
