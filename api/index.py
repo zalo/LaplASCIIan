eac28b57d55dc0b20c0178cd907c694f95c1cf1f
Johnathon Selstad <makeshifted@gmail.com>
2020-02-13 21:46:51 -0800

    Hopefully allow Python to tell me what went wrong...

--- api/index.py
+++ api/index.py
@@ -3,14 +3,25 @@ import json
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Content-type','text/plain')
-        self.end_headers()
+  def do_POST(self):
 
-        # Attempt to read the body of the request
-        body = json.loads(self.rfile.read(int(self.headers.get("Content-Length"))))
 
-        message = 'Hello from Python from a ZEIT Now Serverless Function!   You submitted: '+str(body["gifUrl"]) +" with a density of " + body["density"]
-        self.wfile.write(message.encode())
-        return
+    message = "Conversion Failed for mysterious reasons... try something different?"
+    try:
+      # Attempt to read the body of the request
+      body = json.loads(self.rfile.read(int(self.headers.get("Content-Length"))).decode("utf-8"))
+
+      # Construct a Reponse
+      message = 'Hello from Python from a ZEIT Now Serverless Function!   You submitted: ' + str(body)  # +" with a density of " + body["density"]
+      
+      self.send_response(200)
+      self.send_header('Content-type','text/plain')
+      self.end_headers()
+    except Exception as e:
+      message = "Conversion Failed; Error ({0}): {1}".format(e.errno, e.strerror)
+
+      self.send_response(400)
+    finally:
+      self.wfile.write(message.encode())
+
+    return
