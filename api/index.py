8e32430c5959153b5814967f6205e3fcdbf6d6bb
Johnathon Selstad <makeshifted@gmail.com>
2020-02-13 21:19:42 -0800

    Switch to POSTing...

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
